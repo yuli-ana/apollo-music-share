903a6b52b8bcd18db339862ab2276dfa91d5f1e6
Yuliana Hazda <yulianahazda@gmail.com>
2020-08-13 17:47:34 -0400

    Import themeProvider, pass prop to share a theme with all components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { MuiThemeProvider, CssBaseline } from '@material-ui/core';
+import theme from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MuiThemeProvider theme={theme}>
+      {/* CssBaseline enables to have dark theme */}
+      <CssBaseline /> 
+      <App />
+    </MuiThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
