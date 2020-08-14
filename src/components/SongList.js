f2b902310aea906fa60cf7189f0b3045aff8e7b1
Yuliana Hazda <yulianahazda@gmail.com>
2020-08-14 12:06:36 -0400

    Add a control flow, render a spinner if data is loading

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,6 +1,23 @@
 import React from 'react';
+import { CircularProgress } from '@material-ui/core';
 
 function SongList() {
+    let loading = false;
+
+    // If true show loading spinner
+    if (loading) {
+        return (
+            <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                marginTop: 50
+            }}>
+                <CircularProgress />
+            </div>
+        )
+    }
+
     return (<div>Song List</div>)
 }
 
