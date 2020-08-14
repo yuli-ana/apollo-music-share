6e45b3d327d737aa01158d01057d0a6e62cba52e
Yuliana Hazda <yulianahazda@gmail.com>
2020-08-14 15:11:53 -0400

    Add custom styles

--- src/components/SongPlayer.js
+++ src/components/SongPlayer.js
@@ -1,14 +1,44 @@
 import React from 'react';
 import QueuedSongList from './QueuedSongList';
-import { Card, CardContent, Typography, IconButton, Slider, CardMedia } from '@material-ui/core';
+import { Card, CardContent, Typography, IconButton, Slider, CardMedia, makeStyles } from '@material-ui/core';
 import { SkipPrevious, PlayArrow, SkipNext } from '@material-ui/icons';
 
+const useStyles = makeStyles(theme => ({
+    container: {
+        display: 'flex',
+        justifyContent: 'space-between',
+    },
+    details: {
+        display: 'flex',
+        flexDirection: 'column',
+        padding: '0px 15px',
+    },
+    content: {
+        flex: '1 0 auto',
+    },
+    thumbnail: {
+        width: 150,
+    },
+    controls: {
+        display: 'flex',
+        alignItems: 'center',
+        paddingLeft: theme.spacing(1),
+        paddingRight: theme.spacing(1)
+    },
+    playIcon: {
+        height: 38,
+        width: 38
+    }
+}))
+
 function SongPlayer() {
+    const classes = useStyles();
+
     return (
         <>
-            <Card variant="outlined">
-                <div>
-                    <CardContent>
+            <Card variant="outlined" className={classes.container}>
+                <div className={classes.details}>
+                    <CardContent className={classes.content}>
                         <Typography variant="h5" component="h3">
                             Title
                         </Typography>
@@ -16,12 +46,12 @@ function SongPlayer() {
                             Artist
                         </Typography>
                     </CardContent>
-                    <div>
+                    <div className={classes.controls}>
                         <IconButton>
                             <SkipPrevious />
                         </IconButton>
                         <IconButton>
-                            <PlayArrow />
+                            <PlayArrow className={classes.playIcon} />
                         </IconButton>
                         <IconButton>
                             <SkipNext />
@@ -38,6 +68,7 @@ function SongPlayer() {
                     />
                 </div>
                 <CardMedia
+                    className={classes.thumbnail}
                     image="https://picsum.photos/id/237/100/100"
                 />
             </Card>
