36a3f0bd77f7b311d249932fcedae3a6330ed4e4
Yuliana Hazda <yulianahazda@gmail.com>
2020-08-21 13:04:14 -0400

    Remove unnecessary fileds

--- src/graphQL/queries.js
+++ src/graphQL/queries.js
@@ -1,17 +1,16 @@
 import { gql } from '@apollo/client';
+// All graphQL queries live here
 
 export const GET_SONGS = gql`
 query getSongs {
     songs(order_by: {created_at: desc}){
-      returning {
         artist
-        created_at
         duration
         id
         thumbnail
         title
         url
-      }
     }
   }
 `;
+
