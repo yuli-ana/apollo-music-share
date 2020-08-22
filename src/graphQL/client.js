4a84a6eb4fd3f6eda207bc7466c492581f7f9a7f
Yuliana Hazda <yulianahazda@gmail.com>
2020-08-22 12:52:29 -0400

    Update client object

--- src/graphQL/client.js
+++ src/graphQL/client.js
@@ -1,8 +1,20 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { WebSocketLink } from 'apollo-link-ws';
 
 const client = new ApolloClient({
-    uri: 'https://welcome-chipmunk-46.hasura.app/v1/graphql',
+    link: new WebSocketLink({
+
+        uri: 'wss://welcome-chipmunk-46.hasura.app/v1/graphql',
+        options: {
+            reconnect: true,
+        }
+    }),
     cache: new InMemoryCache()
-});
+})
+
+// const client = new ApolloClient({
+//     uri: 'https://welcome-chipmunk-46.hasura.app/v1/graphql',
+//     cache: new InMemoryCache()
+// });
 
 export default client;
