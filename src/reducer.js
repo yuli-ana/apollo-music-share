9ebf52f60f1f9e42826047aea2cac5688f724f63
Yuliana Hazda <yulianahazda@gmail.com>
2020-08-25 14:50:13 -0400

    Update the reducer function

--- src/reducer.js
+++ src/reducer.js
@@ -2,7 +2,8 @@ function songReducer(state, action) {
     switch (action.type) {
         case "PLAY_SONG":
             return { ...state, isPlaying: true }
-
+        case "PAUSE_SONG":
+            return { ...state, isPlaying: false }
         default:
             return state;
     }
