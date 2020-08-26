b706eca0b2fd0f6e5631117905a2f3dc456d9129
Yuliana Hazda <yulianahazda@gmail.com>
2020-08-26 17:18:24 -0400

    Write data to the cache

--- src/graphQL/client.js
+++ src/graphQL/client.js
@@ -1,5 +1,11 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 import { WebSocketLink } from 'apollo-link-ws';
+
+// Mutation enables to add/remove items from the queue
+// TypeDefs(type definitions) property tells Apollo client about state I want to manage/ it creates a schema to tell apollo what I want to be querying, data itself, structure of data  and  mutations
+
+
+//Instead of type Song create a custom type "input" object that collects all of the individual arguments that I normally pass to mutation
 
 const client = new ApolloClient({
     link: new WebSocketLink({
@@ -9,8 +15,49 @@ const client = new ApolloClient({
             reconnect: true,
         }
     }),
-    cache: new InMemoryCache()
-})
+    cache: new InMemoryCache(),
+    typeDefs: gql`
+    type Song {
+        id: uuid!,
+        title: String!,
+        artist: String!,
+        thumbnail: String!,
+        duration: Float!,
+        ulr: String!
+    }
+
+    input SongInput {
+        id: uuid!,
+        title: String!,
+        artist: String!,
+        thumbnail: String!,
+        duration: Float!,
+        ulr: String!
+    }
+
+    type Query {
+        queue: [Song]!,
+    }
+
+    type Mutation {
+        addOrRemoveFromQueue(input: SongInput): [Song]!
+    }
+    `
+});
+
+
+client.writeQuery({
+    query: gql`
+      query GetQueue {
+        queue
+      }
+    `,
+    data: {
+        queue: []
+    }
+});
+
+
 
 // const client = new ApolloClient({
 //     uri: 'https://welcome-chipmunk-46.hasura.app/v1/graphql',
