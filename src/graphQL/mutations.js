5eabc8ce8aa2d4ad1438223384e38ac9c61e4fb2
Yuliana Hazda <yulianahazda@gmail.com>
2020-08-27 12:59:50 -0400

    Add a new mutation

--- src/graphQL/mutations.js
+++ src/graphQL/mutations.js
@@ -1,7 +1,14 @@
 import { gql } from '@apollo/client';
 
+//Instead of using all individual arguments will bring in an input
 
-const ADD_SONG = gql`
+export const ADD_OR_REMOVE_FROM_QUEUE = gql`
+mutation addOrRemoveFromQueue($input: SongInput!){
+  addOrRemoveFromQueue(input: $input) @client
+}
+`;
+
+export const ADD_SONG = gql`
 mutation addSong($title: String!, $artist: String!, $thumbnail: String!, $duration: Float!, $url: String!) {
     insert_songs(objects: {url: $url, title: $title, thumbnail: $thumbnail, duration: $duration, artist: $artist}) {
       affected_rows
@@ -9,4 +16,4 @@ mutation addSong($title: String!, $artist: String!, $thumbnail: String!, $durati
   }
 `;
 
-export default ADD_SONG;
+// export default ADD_SONG;
