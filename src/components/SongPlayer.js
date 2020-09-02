719a8cd4f210362b2cbf46b921dd6dc2f828d387
Yuliana Hazda <yulianahazda@gmail.com>
2020-09-01 20:35:30 -0400

    Create a new state to control react player and slider, add events & handlers

--- src/components/SongPlayer.js
+++ src/components/SongPlayer.js
@@ -1,10 +1,11 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import QueuedSongList from './QueuedSongList';
 import { Card, CardContent, Typography, IconButton, Slider, CardMedia, makeStyles } from '@material-ui/core';
 import { SkipPrevious, PlayArrow, SkipNext, Pause } from '@material-ui/icons';
 import { SongContext } from '../App';
 import { useQuery } from '@apollo/client';
 import { GET_QUEUED_SONGS } from '../graphQL/queries';
+import ReactPlayer from 'react-player';
 
 const useStyles = makeStyles(theme => ({
     container: {
@@ -30,19 +31,34 @@ const useStyles = makeStyles(theme => ({
     },
     playIcon: {
         height: 38,
-        width: 38
+        width: 38 
     }
 }))
+
 
 function SongPlayer() {
     const { data } = useQuery(GET_QUEUED_SONGS);
     const { state, dispatch } = useContext(SongContext);
     const classes = useStyles();
+    const [played, setPlayed] = useState(0);
+    const [seeking, setSeeking] = useState(false);
 
 
     function handleTogglePlay() {
         // Toggle state
         dispatch(state.isPlaying ? { type: "PAUSE_SONG" } : { type: "PLAY_SONG" })
+    }
+
+    function handleSeekMouseDown(){
+        setSeeking(true);
+    }
+
+    function handleSeekMouseUp(){
+        setSeeking(false);
+    }
+
+    function handleProgressChange(e, newValue){
+        setPlayed(newValue);
     }
 
     return (
@@ -72,13 +88,25 @@ function SongPlayer() {
                             00:01:30
                         </Typography>
                     </div>
+                    {/* Now slider is controlled by a state , I can't move it*/}
                     <Slider
+                        onMouseDown={handleSeekMouseDown}
+                        onMouseUp={handleSeekMouseUp}
+                        onChange={handleProgressChange}
+                        value={played}
                         type="range"
                         min={0}
                         max={1}
                         step={0.01}
                     />
                 </div>
+                <ReactPlayer 
+                    onProgress = {({ played, playedSeconds }) => {
+                        setPlayed(played);
+                    }}
+                    url={state.song.url} 
+                    playing={state.isPlaying} 
+                    hidden />
                 <CardMedia
                     className={classes.thumbnail}
                     image={state.song.thumbnail}
@@ -90,3 +118,6 @@ function SongPlayer() {
 }
 
 export default SongPlayer;
+
+// React player ((onProgress callback))
+// { played, playedSeconds }  - played show users the song progress
